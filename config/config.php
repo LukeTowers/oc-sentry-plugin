c2a79e8b868f041621e493619f113006536ad313
Luca Sartori <luca@codersmedia.it>
2020-12-28 08:25:06 +0100

    Get config values from env

--- config/config.php
+++ config/config.php
@@ -18,20 +18,22 @@
                 'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
                 // capture release as git sha
-                // 'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+		'release' => env('SENTRY_RELEASE_GIT', false)
+			? trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD'))
+			: env('SENTRY_RELEASE_TAG', null),
 
                 'breadcrumbs' => [
                     // Capture Laravel logs in breadcrumbs
-                    'logs' => true,
+                    'logs' => env('SENTRY_BREADCRUMBS_LOGS', true),
 
                     // Capture SQL queries in breadcrumbs
-                    'sql_queries' => true,
+                    'sql_queries' => env('SENTRY_BREADCRUMBS_QUERIES', true),
 
                     // Capture bindings on SQL queries logged in breadcrumbs
-                    'sql_bindings' => true,
+                    'sql_bindings' => env('SENTRY_BREADCRUMBS_BINDINGS', true),
 
                     // Capture queue job information in breadcrumbs
-                    'queue_info' => true,
+                    'queue_info' => env('SENTRY_BREADCRUMBS_QUEUE', true),
                 ],
             ],
         ],
